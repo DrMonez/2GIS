f4fd023ac7ba4f24f824b0e73f8b9a550efd3cfb
Iost Aleksey <iostaleksey@gmail.com>
2019-06-29 11:56:41 +0700

    Добавлены многопоточные тесты

--- MyCollections/TestMyCollections/TestDoubleKeyDictionary.cs
+++ MyCollections/TestMyCollections/TestDoubleKeyDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyCollections;
 
@@ -11,7 +12,18 @@ namespace TestMyCollections
         public void AddTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            Assert.AreEqual(1, a.Count);
+        }
+
+        [TestMethod]
+        public void AddSameObjectsTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
             Assert.AreEqual(1, a.Count);
         }
 
@@ -19,7 +31,7 @@ namespace TestMyCollections
         public void GetIdTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
             var expected = new Tuple<string, double>("0", 0.5);
             var actual = a.GetById(0);
             Assert.AreEqual(expected, actual);
@@ -29,7 +41,7 @@ namespace TestMyCollections
         public void GetNameTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
             var expected = new Tuple<int, double>(0, 0.5);
             var actual = a.GetByName("0");
             Assert.AreEqual(expected, actual);
@@ -39,7 +51,7 @@ namespace TestMyCollections
         public void ClearTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
             a.Clear();
             var expected = 0;
             var actual = a.Count;
@@ -50,7 +62,7 @@ namespace TestMyCollections
         public void RemoveOneByIdTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
 
             a.Remove(0);
 
@@ -63,7 +75,7 @@ namespace TestMyCollections
         public void RemoveOneByNameTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
 
             a.Remove("0");
 
@@ -76,11 +88,11 @@ namespace TestMyCollections
         public void RemoveByIdTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
-            a.Add(new Tuple<int, string, double>(2, "2", 0.7));
-            a.Add(new Tuple<int, string, double>(1, "1", 0.6));
-            a.Add(new Tuple<int, string, double>(3, "3", 0.8));
-            a.Add(new Tuple<int, string, double>(4, "4", 0.9));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(2, "2", 0.7));
+            a.TryAdd(new Tuple<int, string, double>(1, "1", 0.6));
+            a.TryAdd(new Tuple<int, string, double>(3, "3", 0.8));
+            a.TryAdd(new Tuple<int, string, double>(4, "4", 0.9));
 
             a.Remove(1);
 
@@ -108,11 +120,11 @@ namespace TestMyCollections
         public void RemoveByNameTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
-            a.Add(new Tuple<int, string, double>(2, "2", 0.6));
-            a.Add(new Tuple<int, string, double>(1, "1", 0.7));
-            a.Add(new Tuple<int, string, double>(3, "3", 0.8));
-            a.Add(new Tuple<int, string, double>(4, "4", 0.9));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(2, "2", 0.6));
+            a.TryAdd(new Tuple<int, string, double>(1, "1", 0.7));
+            a.TryAdd(new Tuple<int, string, double>(3, "3", 0.8));
+            a.TryAdd(new Tuple<int, string, double>(4, "4", 0.9));
 
             a.Remove("1");
 
@@ -140,11 +152,11 @@ namespace TestMyCollections
         public void RemoveLastTest()
         {
             var a = new DoubleKeyDictionary<int, string, double>();
-            a.Add(new Tuple<int, string, double>(0, "0", 0.5));
-            a.Add(new Tuple<int, string, double>(1, "1", 0.7));
-            a.Add(new Tuple<int, string, double>(2, "2", 0.6));
-            a.Add(new Tuple<int, string, double>(3, "3", 0.8));
-            a.Add(new Tuple<int, string, double>(4, "4", 0.9));
+            a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+            a.TryAdd(new Tuple<int, string, double>(1, "1", 0.7));
+            a.TryAdd(new Tuple<int, string, double>(2, "2", 0.6));
+            a.TryAdd(new Tuple<int, string, double>(3, "3", 0.8));
+            a.TryAdd(new Tuple<int, string, double>(4, "4", 0.9));
 
             a.Remove("4");
 
@@ -173,15 +185,81 @@ namespace TestMyCollections
         {
             var a = new DoubleKeyDictionary<int, string, double>();
 
-            for (var i = 0; i < 10e6; i++)
-                a.Add(i, i.ToString(), i * 0.0001);
+            for (var i = 0; i < 10e5; i++)
+                a.TryAdd(i, i.ToString(), i * 0.0001);
             
-            var expectedCount = 10e6;
+            var expectedCount = 10e5;
             var actualCount = a.Count;
             Assert.AreEqual(expectedCount, actualCount);
 
             var elem = a.GetByName("0");
             Assert.AreEqual(new Tuple<int, double>(0, 0), elem);
+
+        }
+
+        [TestMethod]
+        public void ConcurrentAddTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+
+            var threads = new Thread[10];
+            for (var i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => a.TryAdd(i, i.ToString(), i));
+                threads[i].Start();
+            }
+            foreach(var thread in threads)
+                thread.Join();
+            
+            var expectedCount = 10;
+            var actualCount = a.Count;
+            Assert.AreEqual(expectedCount, actualCount);
+
+        }
+
+        [TestMethod]
+        public void ConcurrentRemoveTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+
+            for (var i = 0; i < 15; i++)
+                a.TryAdd(i, i.ToString(), i);
+
+            var threads = new Thread[10];
+            for (var i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => a.Remove(3));
+                threads[i].Start();
+            }
+            foreach (var thread in threads)
+                thread.Join();
+            
+            var expectedCount = 14;
+            var actualCount = a.Count;
+            Assert.AreEqual(expectedCount, actualCount);
+
+        }
+
+        [TestMethod]
+        public void ConcurrentClearTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+
+            for (var i = 0; i < 3; i++)
+                a.TryAdd(i, i.ToString(), i);
+
+            var threads = new Thread[4];
+            for (var i = 0; i < threads.Length; i++)
+            {
+                threads[i] = i % 2 == 1 ? new Thread(() => a.TryAdd(i, i.ToString(), i)) : new Thread(() => a.Clear());
+                threads[i].Start();
+            }
+            foreach (var thread in threads)
+                thread.Join();
+
+            var expectedCount = 1;
+            var actualCount = a.Count;
+            Assert.AreEqual(expectedCount, actualCount);
 
         }
     }
