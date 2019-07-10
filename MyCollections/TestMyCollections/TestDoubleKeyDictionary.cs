b8ffdbb22ac5f68b13322132298e5f76757c656e
Iost Aleksey <iostaleksey@gmail.com>
2019-07-10 16:07:18 +0700

    Update TestDoubleKeyDictionary.cs

--- MyCollections/TestMyCollections/TestDoubleKeyDictionary.cs
+++ MyCollections/TestMyCollections/TestDoubleKeyDictionary.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyCollections;
@@ -10,157 +11,175 @@ namespace TestMyCollections
     [TestClass]
     public class TestDoubleKeyDictionary
     {
-        //[TestMethod]
-        //public void AddTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    Assert.AreEqual(1, a.Count);
-        //}
+        [TestMethod]
+        public void AddTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.0);
+            Assert.AreEqual(1, a.Count);
+        }
 
-        //[TestMethod]
-        //public void AddSameObjectsTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    Assert.AreEqual(1, a.Count);
-        //}
+        [TestMethod]
+        public void AddSameObjectsTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.0);
+            a.TryAdd(0, "0", 0.0);
+            a.TryAdd(0, "0", 0.0);
+            a.TryAdd(0, "0", 0.0);
+            Assert.AreEqual(1, a.Count);
+        }
 
-        //[TestMethod]
-        //public void AddSameIdTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "1", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "2", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "3", 0.5));
-        //    Assert.AreEqual(4, a.Count);
-        //}
+        [TestMethod]
+        public void AddSameIdTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.TryAdd(0, "1", 0.5);
+            a.TryAdd(0, "2", 0.5);
+            a.TryAdd(0, "3", 0.5);
+            Assert.AreEqual(4, a.Count);
+        }
 
-        //[TestMethod]
-        //public void AddSameNamesTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(1, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(2, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(3, "0", 0.5));
-        //    Assert.AreEqual(4, a.Count);
-        //}
+        [TestMethod]
+        public void AddSameNamesTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.TryAdd(1, "0", 0.5);
+            a.TryAdd(2, "0", 0.5);
+            a.TryAdd(3, "0", 0.5);
+            Assert.AreEqual(4, a.Count);
+        }
 
-        //[TestMethod]
-        //public void GetIdTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    var expected = new Tuple<string, double>[] { new Tuple<string, double>("0", 0.5) };
-        //    var actual = a.GetById(0);
-        //    Assert.AreEqual(expected.Length, actual.Length);
-        //    for (var i = 0; i < actual.Length; i++)
-        //        Assert.AreEqual(expected[i], actual[i]);
-        //}
+        [TestMethod]
+        public void GetIdTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            var expected = new Tuple<string, double>[] { new Tuple<string, double>("0", 0.5) };
+            var actual = a.GetById(0);
+            Assert.AreEqual(expected.Length, actual.Count);
+            var keys = new string[1];
+            actual.Keys.CopyTo(keys, 0);
+            var values = new double[1];
+            actual.Values.CopyTo(values, 0);
+            for (var i = 0; i < keys.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Item1, keys[i]);
+                Assert.AreEqual(expected[i].Item2, values[i]);
+            }
+        }
 
-        //[TestMethod]
-        //public void GetSameIdTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "1", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "2", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(0, "3", 0.5));
-        //    var expected = new Tuple<string, double>[] 
-        //    {
-        //        new Tuple<string, double>("0", 0.5),
-        //        new Tuple<string, double>("1", 0.5),
-        //        new Tuple<string, double>("2", 0.5),
-        //        new Tuple<string, double>("3", 0.5)
-        //    };
-        //    var actual = a.GetById(0);
-        //    Assert.AreEqual(expected.Length, actual.Length);
-        //    for (var i = 0; i < actual.Length; i++)
-        //        Assert.AreEqual(expected[i], actual[i]);
-        //}
+        [TestMethod]
+        public void GetSameIdTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.TryAdd(0, "1", 0.5);
+            a.TryAdd(0, "2", 0.5);
+            a.TryAdd(0, "3", 0.5);
+            var expected = new Dictionary<string, double>()
+            {
+                { "0", 0.5 },
+                { "1", 0.5 },
+                { "2", 0.5 },
+                { "3", 0.5 }
+            };
+            var actual = a.GetById(0);
+            Assert.AreEqual(expected.Count, actual.Count);
+            var keys = new string[4];
+            actual.Keys.CopyTo(keys, 0);
+            for (var i = 0; i < actual.Count; i++)
+                Assert.AreEqual(expected[keys[i]], actual[keys[i]]);
+        }
 
-        //[TestMethod]
-        //public void GetNameTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    var expected = new Tuple<int, double>[] { new Tuple<int, double>(0, 0.5) };
-        //    var actual = a.GetByName("0");
-        //    Assert.AreEqual(expected.Length, actual.Length);
-        //    for (var i = 0; i < actual.Length; i++)
-        //        Assert.AreEqual(expected[i], actual[i]);
-        //}
+        [TestMethod]
+        public void GetNameTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            var expected = new Dictionary<int, double>() { { 0, 0.5 } };
+            var actual = a.GetByName("0");
+            Assert.AreEqual(expected.Count, actual.Count);
+            var keys = new int[1];
+            actual.Keys.CopyTo(keys, 0);
+            for (var i = 0; i < actual.Count; i++)
+                Assert.AreEqual(expected[keys[i]], actual[keys[i]]);
+        }
 
-        //[TestMethod]
-        //public void GetSameNameTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(1, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(2, "0", 0.5));
-        //    a.TryAdd(new Tuple<int, string, double>(3, "0", 0.5));
-        //    var expected = new Tuple<int, double>[] 
-        //    {
-        //        new Tuple<int, double>(0, 0.5),
-        //        new Tuple<int, double>(1, 0.5),
-        //        new Tuple<int, double>(2, 0.5),
-        //        new Tuple<int, double>(3, 0.5)
-        //    };
-        //    var actual = a.GetByName("0");
-        //    Assert.AreEqual(expected.Length, actual.Length);
-        //    for (var i = 0; i < actual.Length; i++)
-        //        Assert.AreEqual(expected[i], actual[i]);
-        //}
+        [TestMethod]
+        public void GetSameNameTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.TryAdd(1, "0", 0.5);
+            a.TryAdd(2, "0", 0.5);
+            a.TryAdd(3, "0", 0.5);
+            var expected = new Dictionary<int, double>()
+            {
+                { 0, 0.5 },
+                { 1, 0.5 },
+                { 2, 0.5 },
+                { 3, 0.5 }
+            };
+            var actual = a.GetByName("0");
+            Assert.AreEqual(expected.Count, actual.Count);
+            var keys = new int[1];
+            actual.Keys.CopyTo(keys, 0);
+            for (var i = 0; i < actual.Count; i++)
+                Assert.AreEqual(expected[keys[i]], actual[keys[i]]);
+        }
 
-        //[TestMethod]
-        //public void GetNullTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.Remove(0, "0");
-            
-        //    var actual = a.GetByName("0");
-        //    Assert.AreEqual(null, actual);
-        //}
+        [TestMethod]
+        public void GetNullTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.Remove(0, "0");
+            try
+            {
+                a.GetByName("0");
+            }
+            catch(Exception ex)
+            {
+                Assert.IsTrue(ex is KeyNotFoundException);
+            }
+        }
 
-        //[TestMethod]
-        //public void GetByIndexOperator()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    var expected = 0.5;
-        //    var actual = a[0, "0"];
-        //    Assert.AreEqual(expected, actual);
-        //}
+        [TestMethod]
+        public void GetByIndexOperator()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            var expected = 0.5;
+            var actual = a[0, "0"];
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[TestMethod]
-        //public void ClearTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
-        //    a.Clear();
-        //    var expected = 0;
-        //    var actual = a.Count;
-        //    Assert.AreEqual(expected, actual);
-        //}
+        [TestMethod]
+        public void ClearTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
+            a.Clear();
+            var expected = 0;
+            var actual = a.Count;
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[TestMethod]
-        //public void RemoveOneTest()
-        //{
-        //    var a = new DoubleKeyDictionary<int, string, double>();
-        //    a.TryAdd(new Tuple<int, string, double>(0, "0", 0.5));
+        [TestMethod]
+        public void RemoveOneTest()
+        {
+            var a = new DoubleKeyDictionary<int, string, double>();
+            a.TryAdd(0, "0", 0.5);
 
-        //    a.Remove(0, "0");
+            a.Remove(0, "0");
 
-        //    var expected = 0;
-        //    var actual = a.Count;
-        //    Assert.AreEqual(expected, actual);
-        //}
+            var expected = 0;
+            var actual = a.Count;
+            Assert.AreEqual(expected, actual);
+        }
 
         //[TestMethod]
         //public void RemoveTest()
@@ -198,18 +217,25 @@ namespace TestMyCollections
         //    Assert.AreEqual(new Tuple<int, double>(4, 0.9), elem[0]);
         //}
 
-        [TestMethod]
-        public void Test()
-        {
-            var a = new Dictionary<string, int>();
-            a.Add("1", 1);
-            a.Add("1", 1);
+        //[TestMethod]
+        //public void Test()
+        //{
+        //    var a = new IDGenerator();
 
-            var expected = 0;
-            var actual = a.Count;
-            Assert.AreEqual(expected, actual);
-        }
+        //    var b = 0;
+        //    var bb = 0;
+        //    var c = "0";
+
+        //    bool isFirst;
+        //    var d = a.GetId(b, out isFirst);
+        //    var e = a.GetId((b, c), out isFirst);
+        //    var f = a.GetId((b, c), out isFirst);
+
+        //}
+
     }
+
+    
 
 
     //[TestClass]
