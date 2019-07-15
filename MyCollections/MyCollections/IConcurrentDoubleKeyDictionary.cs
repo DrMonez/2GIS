f8f4104d33bfcd9781f98375d98543182c4481ae
Iost Aleksey <iostaleksey@gmail.com>
2019-07-15 14:58:26 +0700

    Create IConcurrentDoubleKeyDictionary.cs

---
+++ MyCollections/MyCollections/IConcurrentDoubleKeyDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollections
+{
+    interface IConcurrentDoubleKeyDictionary<TKeyId, TKeyName, TValue>
+    {
+        int Count { get; }
+        bool TryGetById(TKeyId id, out Dictionary<TKeyName, TValue> result);
+        bool TryGetByName(TKeyName name, out Dictionary<TKeyId, TValue> result);
+        Dictionary<TKeyName, TValue> GetByIdOrAdd(TKeyId id, Func<TKeyId, TKeyName, TValue, TValue> valueFactory);
+        Dictionary<TKeyId, TValue> GetByNameOrAdd(TKeyName name, Func<TKeyId, TKeyName, TValue, TValue> valueFactory);
+        bool TryAdd(TKeyId id, TKeyName name, TValue value);
+        bool TryRemove(TKeyId id, TKeyName name);
+        TValue AddOrUpdate(TKeyId id, TKeyName name, TValue value, Func<TKeyId, TKeyName, TValue, TValue> updateValueFactory);
+        void Clear();
+        ICollection<TKeyId> IdKeys { get; }
+        ICollection<TKeyName> NameKeys { get; }
+        ICollection<TValue> Values { get; }
+    }
+}
