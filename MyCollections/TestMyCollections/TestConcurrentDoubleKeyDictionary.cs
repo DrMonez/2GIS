3a4c5a4e3c376ae997a5df55dc260c58aafb9f33
Iost Aleksey <iostaleksey@gmail.com>
2019-07-22 15:01:38 +0700

    Update TestConcurrentDoubleKeyDictionary.cs

--- MyCollections/TestMyCollections/TestConcurrentDoubleKeyDictionary.cs
+++ MyCollections/TestMyCollections/TestConcurrentDoubleKeyDictionary.cs
@@ -17,15 +17,12 @@ namespace TestMyCollections
         {
             // Создаем конкурентный словарь 
             var cd = new ConcurrentDictionary<string, int>();
-            var xxxx = new DoubleKeyDictionary<int, int, int>();
-
-            xxxx.Add(0, 0, 0);
+            
 
             // Хотим получить элемент с ключом "b", если нет - создаем  
             int value = cd.GetOrAdd("b", (key) => 555);
             // Проверяем: value = 555; 
             value = cd.GetOrAdd("b", -333);
-            var xval = xxxx[0, 0];
             // Параллельно пытаемся обновить элемент с ключом "a" 
             Parallel.For(0, 100000, i =>
             {
@@ -34,15 +31,7 @@ namespace TestMyCollections
                 cd.AddOrUpdate("a", 1, (key, oldValue) => oldValue + 1);
             });
 
-            Parallel.For(0, 100000, i =>
-            {
-                // Если ключа нет – добавляем 
-                // Если есть – обновляем значение 
-                xxxx.Add(i, i, i);
-            });
-
-            var a = cd["a"];
-            var b = cd["b"];
+            Assert.AreEqual(100000, cd["a"]);
         }
     }
 }
