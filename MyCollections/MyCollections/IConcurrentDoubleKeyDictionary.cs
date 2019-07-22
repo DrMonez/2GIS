bbe860eebd0f8f4da1a4c34b4b0fc9d190078e79
Iost Aleksey <iostaleksey@gmail.com>
2019-07-22 15:01:52 +0700

    Update IConcurrentDoubleKeyDictionary.cs

--- MyCollections/MyCollections/IConcurrentDoubleKeyDictionary.cs
+++ MyCollections/MyCollections/IConcurrentDoubleKeyDictionary.cs
@@ -9,11 +9,8 @@ namespace MyCollections
         int Count { get; }
         bool TryGetById(TKeyId id, out Dictionary<TKeyName, TValue> result);
         bool TryGetByName(TKeyName name, out Dictionary<TKeyId, TValue> result);
-        Dictionary<TKeyName, TValue> GetByIdOrAdd(TKeyId id, Func<TKeyId, TKeyName, TValue, TValue> valueFactory);
-        Dictionary<TKeyId, TValue> GetByNameOrAdd(TKeyName name, Func<TKeyId, TKeyName, TValue, TValue> valueFactory);
         bool TryAdd(TKeyId id, TKeyName name, TValue value);
         bool TryRemove(TKeyId id, TKeyName name);
-        TValue AddOrUpdate(TKeyId id, TKeyName name, TValue value, Func<TKeyId, TKeyName, TValue, TValue> updateValueFactory);
         void Clear();
         ICollection<TKeyId> IdKeys { get; }
         ICollection<TKeyName> NameKeys { get; }
