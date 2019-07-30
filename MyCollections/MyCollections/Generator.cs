fbce89de1a2c78604e7a3cd6603512560a429a0b
Iost Aleksey <iostaleksey@gmail.com>
2019-07-30 16:13:23 +0700

    Добавлен ConcurrentIDGenerator

--- MyCollections/MyCollections/Generator.cs
+++ MyCollections/MyCollections/Generator.cs
@@ -24,4 +24,29 @@ namespace MyCollections
             return d[o] = gid++;
         }
     }
+
+    internal class ConcurrentIDGenerator
+    {
+        private Dictionary<object, long> _dictionary = new Dictionary<object, long>();
+        private long _number = 0;
+        private object _lockobject = new object();
+
+        public long GetId(object o, out bool isFirst)
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException("0");
+            }
+            lock (_lockobject)
+            {
+                if (_dictionary.ContainsKey(o))
+                {
+                    isFirst = false;
+                    return _dictionary[o];
+                }
+                isFirst = true;
+                return _dictionary[o] = _number++;
+            }
+        }
+    }
 }
