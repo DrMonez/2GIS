c81ef0b69dde976f8186c7cbce549dcf656a8cca
Iost Aleksey <iostaleksey@gmail.com>
2019-07-30 16:12:48 +0700

    Create Constants.cs

---
+++ MyCollections/MyCollections/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollections
+{
+    static class Constants
+    {
+        public const int MaxThreadsCount = 31;
+    }
+}
