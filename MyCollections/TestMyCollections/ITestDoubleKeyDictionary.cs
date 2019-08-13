3c38673efc690fda918180ff7e9663128c8d28c1
DrMonez <iostaleksey@gmail.com>
2019-08-14 00:04:42 +0700

    Добавлен интерфейс тестового класса

---
+++ MyCollections/TestMyCollections/ITestDoubleKeyDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyCollections
+{
+    internal interface ITestDoubleKeyDictionary
+    {
+        void IdKeysTest();
+        void NameKeysTest();
+        void ValuesTest();
+        void AddTest();
+        void AddSameTypesTest();
+        void AddSameObjectsTest();
+        void AddSameIdTest();
+        void AddSameNamesTest();
+        void GetIdTest();
+        void GetSameIdTest();
+        void GetNameTest();
+        void GetSameNameTest();
+        void GetNullTest();
+        void GetByIndexOperator();
+        void ClearTest();
+        void RemoveOneTest();
+        void RemoveTest();
+    }
+}
