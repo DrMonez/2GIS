2e34478d90699e174b529a0c4c02547c2ce0a4b3
DrMonez <iostaleksey@gmail.com>
2019-08-16 17:56:08 +0700

    Переименование файла

---
+++ MyCollections/MyCollections/IDGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCollections
+{
+    internal class IDGenerator<T>
+    {
+        private Dictionary<T, long> _dictionary = new Dictionary<T, long>();
+        private long _number = 0;
+
+        public long GetId(T key, out bool isFirst)
+        {
+            if(key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (_dictionary.ContainsKey(key))
+            {
+                isFirst = false;
+                return _dictionary[key];
+            }
+            isFirst = true;
+            return _dictionary[key] = _number++;
+        }
+
+        public void Remove(T key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary.Remove(key);
+            }
+        }
+    }
+}
