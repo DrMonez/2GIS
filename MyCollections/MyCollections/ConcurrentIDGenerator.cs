22f2f5ed2eadfafb9167a5f318a156d2c6727a32
DrMonez <iostaleksey@gmail.com>
2019-08-16 17:56:13 +0700

    Create ConcurrentIDGenerator.cs

---
+++ MyCollections/MyCollections/ConcurrentIDGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollections
+{
+    internal class ConcurrentIDGenerator<T>
+    {
+        private Dictionary<T, long> _dictionary = new Dictionary<T, long>();
+        private long _number = 0;
+        private object _lockobject = new object();
+
+        public long GetId(T key, out bool isFirst)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            lock (_lockobject)
+            {
+                if (_dictionary.ContainsKey(key))
+                {
+                    isFirst = false;
+                    return _dictionary[key];
+                }
+                isFirst = true;
+                return _dictionary[key] = _number++;
+            }
+        }
+
+        public void Remove(T key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            lock (_lockobject)
+            {
+                if (_dictionary.ContainsKey(key))
+                {
+                    _dictionary.Remove(key);
+                }
+            }
+        }
+    }
+}
